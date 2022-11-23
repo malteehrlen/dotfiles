e0b6948b7ae45b02fb0917f9d1440c6279a0fb95
Malte Ehrlen <malte.ehrlen@vincit.com>
2022-11-23 09:33:19 +0200

    add NERDTreeFind keybind

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -20,8 +20,10 @@ function M.setAllKeymaps()
         M.map("n", "<C-o>", ":Rg<Cr>")
         _G.rgVisual = require('user.rgVisual').ripgrepVisual
         M.map("v", "<C-o>",":lua rgVisual()<CR>", { silent = true })
-        M.map("n", "<leader>m", ":NERDTreeFocus<CR>")
         M.map("n", "<leader>K", ":sp<CR>:execute('resize ' . line('$'))<CR><C-w>j")
+        -- NERDTree
+        M.map("n", "<leader>m", ":NERDTreeFocus<CR>")
+        M.map("n", "<leader>n", ":NERDTreeFind<CR>")
 end
 
 return M
