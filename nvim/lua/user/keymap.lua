722444c826d87dc60b445a90527f325e377877a3
Malte Ehrlen <malte.ehrlen@freiheit.com>
2024-11-27 09:01:18 +0200

    new key bindings

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -6,24 +6,30 @@ function M.map(mode, lhs, rhs, opts)
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
 function M.setAllKeymaps()
         vim.g.mapleader = ','
         -- json format
-        M.map("n", "<leader>j", ":%!python -m json.tool<CR>:set syntax=json<CR>")
-
-        -- jumping
+        M.map("n", "<leader>j", ":%!python3 -m json.tool<CR>:set syntax=json<CR>")
+        -- jump list backwards
         M.map("n", "<leader>,", "<C-o>")
+        -- jump list forwards
         M.map("n", "<leader>.", "<C-i>")
-        -- searching
+        -- ripgrep filenames
         M.map("n", "<C-p>", ":Files<Cr>")
+        -- ripgrep in file content
         M.map("n", "<C-o>", ":Rg<Cr>")
+        -- ripgrep visual selection in all files
         _G.rgVisual = require('user.rgVisual').ripgrepVisual
         M.map("v", "<C-o>",":lua rgVisual()<CR>", { silent = true })
+        -- open floating doc window as permanent split
         M.map("n", "<leader>K", ":sp<CR>:execute('resize ' . line('$'))<CR><C-w>j")
         -- NERDTree
         M.map("n", "<leader>m", ":NERDTreeFocus<CR>")
         M.map("n", "<leader>n", ":NERDTreeFind<CR>")
+        -- copy selection and wrap in triple single qoutes into system buffer, slack code block style
+        M.map("v", "<leader>y", "\"*y<CR>:let @* = \"```\\n\" . @* . \"```\"<CR>", { silent = true })
+        -- copy file path and line number into system buffer like so: `lua/user/keymap.lua:31`
+        M.map("n", "<leader>F", ":let @* = \"`\" . expand(\"%\") . \":\" . line(\".\") . \"`\"<CR>", { silent = true })
 end
 
 return M
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,61 +1,63 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins 
-  use 'scrooloose/nerdtree'
-  use 'tpope/vim-surround'
+    use 'wbthomason/packer.nvim'
+    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+    use 'scrooloose/nerdtree'
+    use 'tpope/vim-surround'
 
-  -- Version Control
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
+    -- Version Control
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
 
-  -- Navigation
-  use 'Xuyuanp/nerdtree-git-plugin'
-  use 'christoomey/vim-tmux-navigator'
-  
-  -- Fuzzy Find
-  use 'junegunn/fzf'
-  use 'junegunn/fzf.vim'
+    -- Navigation
+    use 'Xuyuanp/nerdtree-git-plugin'
+    use 'christoomey/vim-tmux-navigator'
 
-  -- Airline and theme
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'altercation/vim-colors-solarized'
+    -- Fuzzy Find
+    use { 'junegunn/fzf',
+        tag = '0.47.0'
+    }
+    use 'junegunn/fzf.vim'
 
-  -- Completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'saadparwaiz1/cmp_luasnip'
-  use "hrsh7th/cmp-nvim-lsp"
+    -- Airline and theme
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+    use 'altercation/vim-colors-solarized'
 
-  -- Snippets
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
-  
-  --LSP
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'nvim-treesitter/nvim-treesitter'
+    -- Completion
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'saadparwaiz1/cmp_luasnip'
+    use "hrsh7th/cmp-nvim-lsp"
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- Snippets
+    use "L3MON4D3/LuaSnip"
+    use "rafamadriz/friendly-snippets"
+
+    --LSP
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-treesitter/nvim-treesitter'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
